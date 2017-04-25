c7a2fffa3a031e7b26ff1d6e2c60fcd98b616ce4
AlvyS <AlvyShih@hotmail.com>
2017-04-25 21:17:26 +0000

    refactored into seperate functions for search+query and log

--- famous_people.sql
+++ famous_people.sql
@@ -11,3 +11,5 @@ INSERT INTO famous_people (first_name, last_name, birthdate)
   VALUES ('Mahatma', 'Gandhi', '1869-10-02');
 INSERT INTO famous_people (first_name, last_name, birthdate)
   VALUES ('Paul', 'Rudd', '1969-04-06');
+  INSERT INTO famous_people (first_name, last_name, birthdate)
+  VALUES ('Paul', 'Radd', '1969-04-07');
--- lookup_people.js
+++ lookup_people.js
@@ -1,6 +1,7 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
+//----------------------------------------- Connects to default PSQL Vagrant DB -------------------------------------------// 
 const client = new pg.Client({
   user     : settings.user,
   password : settings.password,
@@ -10,18 +11,36 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
-// console.log('client: ', client);
 
-client.connect((err) => {
+const input = process.argv[2];
+
+
+//-------------------------------------------- Connects to DB and Queries ------------------------------------------------//
+function findName(name, callback) {
+  client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT * from famous_people WHERE first_name = $1::text OR last_name = $1::text", [process.argv[2]], (err, result) => {
-      //  console.log('result: ', result);
+  console.log("Searching...")
+  client.query("SELECT * from famous_people WHERE first_name = $1::text OR last_name = $1::text", [input], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows); //output: 1
+    callback(result.rows);
     client.end();
+    });
   });
-});
+}
+
+
+//------------------------------ Used as callback, logs results of queries to required format ----------------------------//
+function printName(resultList) {
+  console.log(`Found ${resultList.length} resultList(s) by the name '${input}'`);
+  for (let i = 0; i < resultList.length; i++) { //  for in loop results in logging 01, 11, 21, 31
+    console.log(`- ${[i + 1]}: ${resultList[i].first_name} ${resultList[i].last_name}, born '${resultList[i].birthdate}'`);
+  }
+}
+
+
+//----- call function + cb
+findName(input, printName);
